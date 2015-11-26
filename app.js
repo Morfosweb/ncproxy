d8c1dba72212037c16dc3957cf35f74eed1a6887
scarabele <henriquescarabele@hotmail.com>
2015-11-26 10:35:04 -0200

    proxy

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var proxy = require('redbird')({port: 80});
+proxy.register("congtool.ncllabs.com", "http://127.0.0.1:8001");
 //proxy.register("congtool.ncllabs.com", "http://127.0.0.1:8001");
-proxy.register("congtool.ncllabs.com", "http://globoesporte.globo.com/");
