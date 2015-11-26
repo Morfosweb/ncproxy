fd28f9fc57de6c856e3306d3b58e36b1be6dd0eb
scarabele <henriquescarabele@hotmail.com>
2015-11-26 10:05:50 -0200

    proxy

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var proxy = require('redbird')({port: 80});
-proxy.register("congtool.ncllabs.com", "http://127.0.0.1:8001");
-console.log("####### init proxy ######");
+proxy.register("congtool.ncllabs.com", "http://127.0.0.1:1234");
+//console.log("####### init proxy ######");
